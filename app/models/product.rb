92f8ec0cd292982378389e16ff05e517e27ca1a9
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-18 09:06:44 +0100

    setup active storage and add many photo attachments to product fixes formating in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.0}
+  validates :price, numericality: { greater_than_or_equal_to: 0.0 }
+  has_many_attached :photos
 end
