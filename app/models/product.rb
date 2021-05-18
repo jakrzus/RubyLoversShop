6828fe539f3fea25abacf66e30b59bb8a41e2a2c
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-18 14:20:40 +0100

    create category model add category_id to product and create one to many relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.0 }
   has_many_attached :photos
+  belongs_to :category
 end
