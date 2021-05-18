9925b9f11c6b690a05aab7e81a46be44a3dbdb76
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-18 17:33:53 +0100

    add filter form to home page so user can filter products via category, add filter to pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@
 
 class PagesController < ApplicationController
   def home
-    @products = Product.all
+    @products = Product.where(query_params[:filter])
+    @categories = Category.all
   end
+end
+
+private
+
+def query_params
+  params.permit(filter: [:category_id])
 end
