4b1a7ddb7f71a2397525fb22626f09a2cc3463f0
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-18 13:48:40 +0100

    rubocop style fixes

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     price { '9.99' }
     trait :with_photo do
       after(:create) do |product|
-      file = File.open(Rails.root.join('spec', 'support', 'watch.webp'))
-      product.photos.attach(io: file, filename: 'watch.webp', content_type: 'image/webp')
+        file = File.open(Rails.root.join('spec/support/watch.webp'))
+        product.photos.attach(io: file, filename: 'watch.webp', content_type: 'image/webp')
       end
     end
   end
