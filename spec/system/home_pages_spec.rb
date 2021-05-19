ad1fe56f2b4cbaa40b7067a5f92d9afbc83c56a0
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-19 19:49:50 +0100

    remove unnessesary expectations

--- spec/system/home_pages_spec.rb
+++ spec/system/home_pages_spec.rb
@@ -13,11 +13,5 @@ RSpec.describe 'HomePages', type: :system do
   it 'shows all products' do
     visit root_path
     expect(page.all('.product-card').count).to eq(10)
-    expect(page.all('.product-card-img').count).to eq(5)
-    expect(page.all('.product-card-placeholder-img').count).to eq(5)
-    Product.all.each do |product|
-      expect(page).to have_content(product.name)
-      expect(page).to have_content(product.price)
-    end
   end
 end
