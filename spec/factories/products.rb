fc1301ae4f6e6111a002203c6a717c3930da25db
jakrzus <jakub.krzus@gmail.com>
2021-05-20 10:01:49 +0100

    Product list on home page (#1)
    * set ruby version to 2.7.3
    
    * create product model with name:string and price:decimal
    
    * setup active storage and add many photo attachments to the product
    fixes formatting in product model
    
    * add capybara to Gemfile
    
    * create product factory and system spec testing product visibility on the homepage
    
    * add :with_photo trait to product factory and sample photo. Updates home_page system spec accordingly
    
    * create 'products/_product' partial, add @products to 'pages#home' and
    display all products on home page
    
    * bump ruby version in github quality checks as it was incompatible with the one in Gemfile
    
    * Delete product_spec.rb

---
+++ spec/factories/products.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :product do
+    sequence(:name) { |n| "Product#{n}" }
+    price { '9.99' }
+    trait :with_photo do
+      after(:create) do |product|
+        file = File.open(Rails.root.join('spec/support/watch.webp'))
+        product.photos.attach(io: file, filename: 'watch.webp', content_type: 'image/webp')
+      end
+    end
+  end
+end
---
+++ spec/system/home_pages_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'HomePages', type: :system do
+  before do
+    driven_by(:rack_test)
+    Product.destroy_all
+    create_list(:product, 5, :with_photo)
+    create_list(:product, 5)
+  end
+
+  it 'shows all products' do
+    visit root_path
+    expect(page.all('.product-card').count).to eq(10)
+  end
+end
