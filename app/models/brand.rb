6a5fdf4bfcf73b6664cb4856f15b76c3a7109a0b
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-21 13:25:06 +0100

    add dependent: :restrict_with_exception to brand model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Brand < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :restrict_with_exception
   validates :name, presence: true, uniqueness: true
   def self.select_params
     pluck(:name, :id)
