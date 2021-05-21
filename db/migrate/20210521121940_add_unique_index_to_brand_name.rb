6cc78ccead9a802af37235a9f43304005102426e
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-21 13:22:35 +0100

    add unique index to category :name

---
+++ db/migrate/20210521121940_add_unique_index_to_brand_name.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToBrandName < ActiveRecord::Migration[6.1]
+  def change
+    add_index :brands, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_093951) do
+ActiveRecord::Schema.define(version: 2021_05_21_121940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_093951) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_brands_on_name", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
