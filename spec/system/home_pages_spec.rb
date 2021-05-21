a1af859ff4f7a597cab13c71909ad6b196da3164
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-21 15:05:41 +0100

    refactor home_pages_spec

--- spec/system/home_pages_spec.rb
+++ spec/system/home_pages_spec.rb
@@ -6,31 +6,31 @@ RSpec.describe 'HomePages', type: :system do
   before do
     driven_by(:rack_test)
     Product.destroy_all
-    create_list(:category, 3)
+    %i[brand category].each { |item| create_list(item, 3) }
     create_list(:product, 5, :with_photo)
     create_list(:product, 5)
+    visit root_path
+  end
+
+  def card_count(page)
+    page.all('.product-card').count
   end
 
   it 'shows all products' do
-    visit root_path
-    expect(page.all('.product-card').count).to eq(10)
+    expect(card_count(page)).to eq(10)
   end
 
   it 'filters products by category' do
-    visit root_path
     category = Category.first
     select category.name, from: 'filter_category_id'
     click_on 'Save Filter'
-
-    expect(page.all('.product-card').count).to eq(category.products.count)
+    expect(card_count(page)).to eq(category.products.count)
   end
 
   it 'filters products by brand' do
-    visit root_path
     brand = Brand.first
     select brand.name, from: 'filter_brand_id'
     click_on 'Save Filter'
-
-    expect(page.all('.product-card').count).to eq(brand.products.count)
+    expect(card_count(page)).to eq(brand.products.count)
   end
 end
