3f5c952fe48806de63e417cab5e602eaf7d51252
Jakub Krzus <jakub.krzus@gmail.com>
2021-05-25 20:34:50 +0100

    add sign in system spec

--- spec/system/authentications_spec.rb
+++ spec/system/authentications_spec.rb
@@ -7,12 +7,21 @@ RSpec.describe 'Authentications', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:user) { build_stubbed :user }
+  let(:new_user) { build_stubbed :user }
+  let(:existing_user) { create :user }
 
   it 'allows to sign up' do
     visit new_user_registration_path
-    fill_in_sign_up_form_as user
+    fill_in_sign_up_form_as new_user
     click_on 'Sign up'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  it 'allows to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: existing_user.email
+    fill_in 'Password', with: existing_user.password
+    click_on 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 end
