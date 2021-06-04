c4a390bb814d00f715e0bb1445a0cea67f9a0817
jakrzus <jakub.krzus@gmail.com>
2021-06-04 17:02:39 +0100

    redirect to home page if logged in user try to access admin panel but is not an admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -4,5 +4,8 @@ module Admin
   class AdminController < ApplicationController
     before_action :authenticate_admin_user!
     layout 'admin'
+    def authenticate_admin_user!
+      current_user.present? ? redirect_to(root_path, alert: 'You are not authorized') : super
+    end
   end
 end
