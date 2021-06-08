de48b04dc535e1aa9cb71aea1307b3f4eba648be
jakrzus <jakub.krzus@gmail.com>
2021-06-08 15:58:39 +0100

    Setup admin dashboard (#9)
    * create admin_user
    
    * create admin namespace and within pages_controller with action dashboard
    
    * add custom layout to admin namespace
    
    * redirect to home page if logged in user try to access admin panel but is not an admin
    
    * add admin layout to admin_user devise pages
    
    * add pry-byebug to gemfile
    
    * add admin dashboard authorization spec
    
    * Fix formatting

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -4,7 +4,7 @@ module Admin
   class AdminController < ApplicationController
     before_action :authenticate_admin_user!
     layout 'admin'
-    
+
     def authenticate_admin_user!
       current_user.present? ? redirect_to(root_path, alert: 'You are not authorized') : super
     end
