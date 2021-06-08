3f892a16d4959aa47140f7d77c90fcf2dff61b89
jakrzus <jakub.krzus@gmail.com>
2021-06-08 20:46:54 +0100

    add product filtering on admin dashboard

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -3,7 +3,13 @@
 module Admin
   class PagesController < AdminController
     def dashboard
-      render :dashboard, locals: { products: Product.all }
+      render :dashboard, locals: { products: Product.filtered(filter_params) }
+    end
+
+    private
+
+    def filter_params
+      params.require(:filter).permit(:category_id, :brand_id) if params[:commit]
     end
   end
 end
