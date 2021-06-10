0bad2ec3b62b8fbc717b411698ad57480063a294
jakrzus <jakub.krzus@gmail.com>
2021-06-10 19:42:15 +0100

    add delete product specs

--- spec/requests/admin_products_spec.rb
+++ spec/requests/admin_products_spec.rb
@@ -11,14 +11,24 @@ RSpec.describe 'AdminProducts', type: :request do
     log_in_admin admin
   end
 
-  let!(:product) { build_stubbed(:product) }
+  let!(:new_product) { build_stubbed :product }
+  let(:existing_product) { create :product }
 
   describe 'POST /admin/products/new' do
     it 'creates new products' do
-      post admin_products_path, params: { product: product.as_json }
+      post admin_products_path, params: { product: new_product.as_json }
       follow_redirect!
 
       expect(response.body).to include('Product was successfully created')
+    end
+  end
+
+  describe 'DELETE /admin/products/:id' do
+    it 'deletes a product' do
+      delete admin_product_path(existing_product)
+      follow_redirect!
+
+      expect(response.body).to include('Product was successfully deleted')
     end
   end
 end
