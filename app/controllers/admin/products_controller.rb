da3f2d1688fed17f32c394d919c8c0fc54da0af0
jakrzus <jakub.krzus@gmail.com>
2021-06-10 18:05:53 +0100

    add delete action and route to product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,10 +9,19 @@ module Admin
     def create
       product = Product.new(product_params)
       if product.save
-        redirect_to root_path, notice: 'Product was successfully created'
+        redirect_to admin_root_path, notice: 'Product was successfully created'
       else
         flash.now[:alert] = 'Product was not created'
         render :new, locals: { product: Product.new(product_params) }
+      end
+    end
+
+    def destroy
+      product = Product.find(params[:id])
+      if product.destroy
+        redirect_to admin_root_path, notice: 'Product was successfully deleted'
+      else
+        redirect_back fallback_location: admin_root_path, alert: 'Could not dlete product'
       end
     end
 
