582b223e178f893bb12e1962757e0d4ff619cb01
jakrzus <jakub.krzus@gmail.com>
2021-06-21 16:58:16 +0100

    Add category brand and product to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,17 @@
 User.create(email: 'email@example', password: 'password')
 AdminUser.create(email: 'admin@example', password: 'password')
+3.times do |n|
+  category = Category.new(name: "Category#{n}")
+  brand = Brand.new(name: "Brand#{n}")
+
+  Category.where(name: category.name).first_or_create(category.as_json)
+  Brand.where(name: brand.name).first_or_create(brand.as_json)
+end
+10.times do |n|
+  product = Product.new(name: "Product#{n}",
+                        price: rand(1000.01..99999.99).round(2),
+                        brand: Brand.all.sample,
+                        category: Category.all.sample )
+                        
+  Product.where(name: product.name).first_or_create(product.as_json)
+end
