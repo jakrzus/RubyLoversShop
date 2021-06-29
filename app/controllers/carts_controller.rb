3e2c248f136de19d383e1ffec9e270497b3fcf48
jakrzus <jakub.krzus@gmail.com>
2021-06-29 19:48:04 +0100

    remove set_cart callback

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  before_action :authenticate_user!, :set_cart
+  before_action :authenticate_user!
   def show
-    render :show, locals: { items: @cart.cart_items }
+    render :show, locals: { items: cart.cart_items }
   end
 
   def add_product
     product = Product.find params[:id]
-    add_product = CartServices::AddProduct.new.call @cart, product
+    add_product = CartServices::AddProduct.new.call cart, product
     if add_product.success?
       redirect_to root_path, notice: 'Product added successfully'
     else
@@ -18,7 +18,7 @@ class CartsController < ApplicationController
 
   private
 
-  def set_cart
-    @cart = current_user.cart
+  def cart
+    @cart ||= current_user.cart
   end
 end
