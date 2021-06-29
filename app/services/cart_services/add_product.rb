1b4d887534f7daaa5c9312e87da2e6c0f61f7413
jakrzus <jakub.krzus@gmail.com>
2021-06-29 21:31:10 +0100

    extract logic to private method in AddProduct service

--- app/services/cart_services/add_product.rb
+++ app/services/cart_services/add_product.rb
@@ -5,10 +5,7 @@ module CartServices
     def call(cart, product)
       if cart_include_product? cart, product
         OpenStruct.new({ success?: false, payload: { error: 'Product allready in the cart' } })
-      elsif item_saved? cart, product
-        OpenStruct.new({ success?: true, payload: @item })
-      else
-        OpenStruct.new({ success?: false, payload: { error: 'Could not add item', errors: @item&.errors } })
+      else save_item cart, product
       end
     end
 
@@ -18,9 +15,14 @@ module CartServices
       cart.cart_items.map(&:product).include? product
     end
 
-    def item_saved?(cart, product)
-      @item = cart.cart_items.build product_id: product.id
-      @item.save
+    def save_item(cart, product)
+      item = cart.cart_items.build product_id: product.id
+
+      if item.save
+        OpenStruct.new({ success?: true, payload: item })
+      else
+        OpenStruct.new({ success?: false, payload: { error: 'Could not add item', errors: item&.errors } })
+      end
     end
   end
 end
