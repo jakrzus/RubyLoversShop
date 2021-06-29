431c9f0e30ab7994df6dd206b8754e12812b7d9f
jakrzus <jakub.krzus@gmail.com>
2021-06-29 20:48:50 +0100

    refactor AddProduct service object

--- app/services/cart_services/add_product.rb
+++ app/services/cart_services/add_product.rb
@@ -3,16 +3,24 @@
 module CartServices
   class AddProduct
     def call(cart, product)
-      if cart.cart_items.map(&:product).include? product
+      if cart_include_product? cart, product
         OpenStruct.new({ success?: false, payload: { error: 'Product allready in the cart' } })
+      elsif item_saved? cart, product
+        OpenStruct.new({ success?: true, payload: @item })
       else
-        item = cart.cart_items.build product_id: product.id
-        if item.save
-          OpenStruct.new({ success?: true, payload: item })
-        else
-          OpenStruct.new({ success?: false, payload: { error: 'Could not add item', errors: item&.errors } })
-        end
+        OpenStruct.new({ success?: false, payload: { error: 'Could not add item', errors: @item&.errors } })
       end
+    end
+
+    private
+
+    def cart_include_product?(cart, product)
+      cart.cart_items.map(&:product).include? product
+    end
+
+    def item_saved?(cart, product)
+      @item = cart.cart_items.build product_id: product.id
+      @item.save
     end
   end
 end
