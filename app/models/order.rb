c06eb37706ef6f543477285f15e2e1ca30a41079
jakrzus <jakub.krzus@gmail.com>
2021-07-03 15:16:45 +0100

    order has many cart items

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@
 
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :cart_items, dependent: :destroy
   enum state: { new: 0, failed: 1, completed: 2 }, _prefix: true, _default: :new
 end
