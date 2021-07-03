4031e2e01eb4eb15774315b9c64fe647d053a51d
jakrzus <jakub.krzus@gmail.com>
2021-07-03 17:25:09 +0100

    redirects to checkout if unfinished check out

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -26,18 +26,28 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    order = current_user.orders.build
-    response = CartServices::CheckOut.new.call cart, order
-    if response.success?
-      redirect_to root_path, notice: 'Order has been created'
-    else
-      redirect_back fallback_location: root_path, alert: response.payload.errors.full_messages
-    end
+    create_new_order unless unfinished_checkout?
   end
 
   private
 
   def cart
     @cart ||= current_user.cart
+  end
+
+  def unfinished_checkout?
+    if current_user.orders.find_by(state: :new).present?
+      redirect_to new_check_out_path, alert: 'You must finish check out'
+    end
+  end
+
+  def create_new_order
+    order = current_user.orders.build
+    response = CartServices::CheckOut.new.call cart, order
+    if response.success?
+      redirect_to new_check_out_path, notice: 'Order has been created'
+    else
+      redirect_back fallback_location: root_path, alert: response.payload.errors.full_messages
+    end
   end
 end
