20fc5e94f759c7955df437948bd96fb7ff9911d2
jakrzus <jakub.krzus@gmail.com>
2021-07-08 19:35:01 +0100

    create GET /admin/orders  request spec and update cart_items and orders factories

--- spec/factories/cart_items.rb
+++ spec/factories/cart_items.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :cart_item do
     before { FactoryBot.create_list(:product, 5) if Product.count < 5 }
 
-    product { Product.all.sample }
+    product { Product.all.sample || FactoryBot.create(:product) }
   end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -4,10 +4,14 @@ STATES = %i[new failed completed].freeze
 
 FactoryBot.define do
   factory :order do
-    user { User.all.sample }
+    user { User.all.sample || FactoryBot.create(:user) }
     state { STATES.sample }
     before(:create) do |order|
       order.cart_items = FactoryBot.build_list(:cart_item, 5)
+    end
+
+    trait :big_id do
+      sequence :id, 99_999
     end
   end
 end
