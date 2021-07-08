40ab02c9b2410dd899f42772978d4f68c90fd90e
jakrzus <jakub.krzus@gmail.com>
2021-07-08 20:57:26 +0100

    associate products to order through cart_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
   enum state: { new: 0, failed: 1, completed: 2 }, _prefix: true, _default: :new
 end
