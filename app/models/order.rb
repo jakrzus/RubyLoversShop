24006b114a935e94126628fd4d0ae98b9fdf9288
jakrzus <jakub.krzus@gmail.com>
2021-07-13 18:46:41 +0100

    create payment model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,5 +6,9 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+  has_one :payment, dependent: :destroy
+
   enum state: STATES, _prefix: true, _default: :new
+
+  before_create :build_payment
 end
