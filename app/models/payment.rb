7b13755bdc38d9b97d0f5a34e8d6cf102c9d7b32
jakrzus <jakub.krzus@gmail.com>
2021-07-13 19:21:34 +0100

    add aasm states and events to payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,14 @@ class Payment < ApplicationRecord
   include AASM
 
   aasm column: 'state' do
+    state :pending, initial: true
+    state :failed, :completed
+    event :cancel do
+      transitions from: :pending, to: :failed
+    end
+    event :complete do
+      transitions from: :pending, to: :completed
+    end
   end
   belongs_to :order
 end
