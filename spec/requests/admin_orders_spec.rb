4259b3d176b710cd97e7a48f2600d1d064501201
jakrzus <jakub.krzus@gmail.com>
2021-07-19 18:35:34 +0100

    create post set_payment request spec

--- spec/requests/admin_orders_spec.rb
+++ spec/requests/admin_orders_spec.rb
@@ -41,8 +41,16 @@ RSpec.describe 'AdminOrders', type: :request do
   describe 'POST /admin/orders/set_payment' do
     let(:order) { create :order }
 
+    before do
+      sign_in admin
+    end
+
     it 'change order status to completed only once' do
       post '/admin/set_payment', params: { payment_id: order.payment.id.to_s, event: :complete }
+      expect(response.body).to include 'Succesfully changed payment status!'
+      post '/admin/set_payment', params: { payment_id: order.payment.id.to_s, event: :cancel }
+
+      expect(response.body).not_to include "Event 'cancel' cannot transition from 'completed'"
     end
   end
 end
