51ff3e458925e48316d4f55c80aba729d71208f9
jakrzus <jakub.krzus@gmail.com>
2021-07-19 23:25:42 +0100

    update tests to include set_payment route change

--- spec/requests/admin_payments_spec.rb
+++ spec/requests/admin_payments_spec.rb
@@ -5,19 +5,18 @@ require './spec/support/helpers'
 
 RSpec.describe 'AdminPayments', type: :request do
   let!(:admin) { create :admin_user }
-  let(:order) { create :order }
+  let!(:payment) { create :payment }
 
-  describe 'POST /admin/orders/set_payment' do
+  describe 'PUT /admin/payments/:id' do
     before do
       sign_in admin
     end
 
     it 'change order status to completed only once' do
-      post '/admin/set_payment', params: { payment_id: order.payment.id.to_s, event: :complete }
-      expect(response.body).to include 'Succesfully changed payment status!'
-      post '/admin/set_payment', params: { payment_id: order.payment.id.to_s, event: :cancel }
-
-      expect(response.body).not_to include "Event 'cancel' cannot transition from 'completed'"
+      put "/admin/payments/#{payment.id}", params: { event: :complete }
+      put "/admin/payments/#{payment.id}", params: { event: :cancel }
+      follow_redirect!
+      expect(response.body).to include CGI.escapeHTML("Event 'cancel' cannot transition from 'completed'")
     end
   end
 end
