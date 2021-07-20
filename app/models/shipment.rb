f753439ae2a3977ec3d478b345205663a86c0831
jakrzus <jakub.krzus@gmail.com>
2021-07-20 22:03:52 +0100

    set up shipment aasm states and events

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,28 @@
+# frozen_string_literal: true
+
 class Shipment < ApplicationRecord
   include AASM
 
-  aasm :column => 'state' do
+  aasm column: 'state' do
+    state :pending, initial: true
+    state :ready, :canceled, :shipped, :failed
+    event :prepare do
+      transitions from: :pending, to: :ready
+    end
+    event :cancel do
+      transitions from: :pending, to: :canceled
+    end
+    event :ship do
+      transitions from: :ready, to: :shipped, guard: :payment_completed?
+    end
+    event :fail do
+      transitions from: :ready, to: :failed
+    end
   end
+
+  def payment_completed?
+    order.payment.completed?
+  end
+
   belongs_to :order
 end
