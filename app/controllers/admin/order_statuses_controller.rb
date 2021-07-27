e62af2d564d15392bf47fa892b377cdcfe8c47d3
jakrzus <jakub.krzus@gmail.com>
2021-07-27 15:53:12 +0100

    create order_statuses controller and route

---
+++ app/controllers/admin/order_statuses_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Admin
+  class OrderStatusesController < StatusesController
+    private
+
+    def resource
+      Order.find(params[:id])
+    end
+  end
+end
