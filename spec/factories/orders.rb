2b12d3bf491ec271e68fca3e6d5f27269fcd19a8
jakrzus <jakub.krzus@gmail.com>
2021-07-27 17:50:42 +0100

    extract  random orders state in factory to a  trait

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :order do
     user { User.all.sample || FactoryBot.create(:user) }
-    state { Order::STATES.sample }
     before(:create) do |order|
       create_list :product, 5
       order.cart_items = build_list(:cart_item, 5)
@@ -11,6 +10,9 @@ FactoryBot.define do
 
     trait :big_id do
       sequence :id, 9_999_999
+    end
+    trait :random_state do
+      state { Order::STATES.sample }
     end
   end
 end
