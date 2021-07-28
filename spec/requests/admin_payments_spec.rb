83b9f45c45eaafb3809df2d013d3874e5addbfd0
jakrzus <jakub.krzus@gmail.com>
2021-07-28 14:53:45 +0100

    add payment request spec to prevent changing state from canceled to completed

--- spec/requests/admin_payments_spec.rb
+++ spec/requests/admin_payments_spec.rb
@@ -12,11 +12,18 @@ RSpec.describe 'AdminPayments', type: :request do
       sign_in admin
     end
 
-    it 'change order status to completed only once' do
+    it 'change payment status to completed only once' do
       put "/admin/payments/#{payment.id}", params: { event: :complete }
       put "/admin/payments/#{payment.id}", params: { event: :cancel }
       follow_redirect!
       expect(response.body).to include CGI.escapeHTML("Event 'cancel' cannot transition from 'completed'")
+    end
+
+    it 'does not change payment status from cancelled to completed' do
+      put "/admin/payments/#{payment.id}", params: { event: :cancel }
+      put "/admin/payments/#{payment.id}", params: { event: :complete }
+      follow_redirect!
+      expect(response.body).to include CGI.escapeHTML(" Event 'complete' cannot transition from 'failed'.")
     end
   end
 end
