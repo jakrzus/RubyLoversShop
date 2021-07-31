306c6e02ceddc9616dadd3e1e3a5343aa798e869
jakrzus <jakub.krzus@gmail.com>
2021-07-31 20:32:19 +0100

    refactor shipment_controller and add json response format

--- app/controllers/admin/shipments_controller.rb
+++ app/controllers/admin/shipments_controller.rb
@@ -3,9 +3,16 @@
 module Admin
   class ShipmentsController < AdminController
     def update
-      result = OrderServices::SetPaymentStatus.new.call shipment, event
-      flash[result.flash[:type]] = result.flash[:message]
-      redirect_to admin_order_path shipment.order
+      result = OrderServices::SetShipmentStatus.new.call shipment, event
+      respond_to do |format|
+        format.html do
+          set_flash_message result, flash
+          redirect_to admin_order_path(order)
+        end
+        format.json do
+          render json: result.payload
+        end
+      end
     end
 
     private
@@ -16,6 +23,14 @@ module Admin
 
     def shipment
       Shipment.find params[:id]
+    end
+
+    def order
+      shipment.order
+    end
+
+    def set_flash_message(result, flash)
+      flash[result.flash[:type]] = result.flash[:message]
     end
   end
 end
