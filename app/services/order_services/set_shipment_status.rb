27d320322bc100504193a9f5281e14aa4c66f631
jakrzus <jakub.krzus@gmail.com>
2021-07-31 20:33:19 +0100

    set-shipment_status.payload returns shipment instead of aasm response

--- app/services/order_services/set_shipment_status.rb
+++ app/services/order_services/set_shipment_status.rb
@@ -3,8 +3,8 @@
 module OrderServices
   class SetShipmentStatus
     def call(shipment, event)
-      response = shipment.aasm.fire! event
-      OpenStruct.new(flash: { type: :notice, message: 'Succesfully changed shipment status' }, payload: response)
+      shipment.aasm.fire!(event)
+      OpenStruct.new(flash: { type: :notice, message: 'Succesfully changed shipment status' }, payload: shipment)
     rescue AASM::InvalidTransition, ActiveModel::ValidationError => e
       OpenStruct.new(flash: { type: :alert, message: e.message }, payload: e)
     end
