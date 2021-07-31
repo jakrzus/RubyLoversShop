7fdc0623ec24e9842903f644ebb07006eab4a2ce
jakrzus <jakub.krzus@gmail.com>
2021-07-31 20:34:05 +0100

    update request shipment spec to json format

--- spec/requests/admin_shipments_spec.rb
+++ spec/requests/admin_shipments_spec.rb
@@ -5,27 +5,44 @@ require './spec/support/helpers'
 
 RSpec.describe 'AdminShipments', type: :request do
   let!(:admin) { create :admin_user }
-  let!(:shipment) { create :shipment }
+
+  headers = { 'ACCEPT' => 'application/json' }
 
   describe 'PUT /admin/shipments/:id' do
     before do
       sign_in admin
     end
 
+    let!(:shipment) { create :shipment }
+
     it 'can not change shipment status to shipped until payment is completed' do
-      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }
-      put "/admin/shipments/#{shipment.id}", params: { event: :ship }
-      follow_redirect!
-      expect(response.body).to include
-      CGI.escapeHTML("Event 'ship' cannot transition from 'ready'. Failed callback(s): [:payment_completed?].")
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+      put "/admin/shipments/#{shipment.id}", params: { event: :ship }, headers: headers
+
+      expect(response.body).to include("Event 'ship' cannot transition from 'ready'.")
     end
 
-    it 'can change shipment status to shipped if payment is completed' do
-      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }
-      put "/admin/payments/#{shipment.payment.id}", params: { event: :complete }
-      put "/admin/shipments/#{shipment.id}", params: { event: :ship }
-      follow_redirect!
-      expect(response.body).to include 'Payment status: completed'
+    it 'can change shipment status from pending to canceled' do
+      put "/admin/shipments/#{shipment.id}", params: { event: :cancel }, headers: headers
+
+      expect(JSON.parse(response.body)['state']).to eq('canceled')
+    end
+
+    it 'can change shipment status from ready to failed' do
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+      expect(JSON.parse(response.body)['state']).to eq('ready')
+      put "/admin/shipments/#{shipment.id}", params: { event: :cancel }, headers: headers
+
+      expect(JSON.parse(response.body)['state']).to eq('failed')
+    end
+
+    it 'can not change shipment status from failed to ready' do
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+      put "/admin/shipments/#{shipment.id}", params: { event: :cancel }, headers: headers
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+
+      expect(JSON.parse(response.body)['state']).not_to eq('ready')
+      expect(response.body).to include("Event 'prepare' cannot transition from 'failed'.")
     end
   end
 end
