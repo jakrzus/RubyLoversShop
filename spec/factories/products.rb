4bcf45e213354723ae9259be2749e42725fa134b
jakrzus <jakub.krzus@gmail.com>
2021-08-01 18:40:53 +0100

    add description to products factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product#{n}" }
     price { '999.99' }
+    description { lorem }
     category { Category.any? ? Category.all.sample : create(:category) }
     brand { Brand.any? ? Brand.all.sample : create(:brand) }
     trait :with_photo do
@@ -16,4 +17,14 @@ FactoryBot.define do
       name { '' }
     end
   end
+end
+def lorem
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  sed do eiusmod tempor incididunt ut labore et dolore magna
+  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit
+  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+  occaecat cupidatat non proident, sunt in culpa qui officia
+  deserunt mollit anim id est laborum.'
 end
