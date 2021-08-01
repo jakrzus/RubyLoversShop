c0f3549c74e25b9299467bdf28fd2a08f6ae3dba
jakrzus <jakub.krzus@gmail.com>
2021-08-01 22:55:34 +0100

    update seeds with product description

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 require 'uri'
-User.create(email: 'email@example', password: 'password')
-AdminUser.create(email: 'admin@example', password: 'password')
-3.times do 
+user = User.new(email: 'email@example', password: 'password')
+admin = AdminUser.new(email: 'admin@example', password: 'password')
+
+User.where(email: user.email).first_or_create(user.as_json)
+AdminUser.where(email: admin.email).first_or_create(admin.as_json)
+3.times do
   category = Category.new(name: Faker::Vehicle.unique.car_type)
   brand = Brand.new(name: Faker::Vehicle.unique.make)
 
@@ -11,11 +14,11 @@ end
 15.times do
   file = URI.open(Faker::Avatar.image)
   product = Product.new(name: Faker::Vehicle.unique.model,
-                        price: rand(1000.01..99999.99).round(2),
+                        price: rand(1000.01..99_999.99).round(2),
                         brand: Brand.all.sample,
-                        category: Category.all.sample )
-                        
+                        category: Category.all.sample,
+                        description: Faker::Lorem.paragraphs(number: 10).join)
+
   product = Product.where(name: product.name).first_or_create(product.as_json)
-  product.photos.attach io: file, filename: "carphoto.jpg", content_type: 'image/jpg'
-  
+  product.photos.attach io: file, filename: 'carphoto.jpg', content_type: 'image/jpg'
 end
