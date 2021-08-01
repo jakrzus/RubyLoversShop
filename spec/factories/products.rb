bc063bf4a47e22123746711371822ab9bb46ef92
jakrzus <jakub.krzus@gmail.com>
2021-08-01 22:54:40 +0100

    fix products factory description formatting

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -18,13 +18,16 @@ FactoryBot.define do
     end
   end
 end
+
 def lorem
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  <<-STRING.squish
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
   sed do eiusmod tempor incididunt ut labore et dolore magna
   aliqua. Ut enim ad minim veniam, quis nostrud exercitation
   ullamco laboris nisi ut aliquip ex ea commodo consequat.
   Duis aute irure dolor in reprehenderit in voluptate velit
   esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
   occaecat cupidatat non proident, sunt in culpa qui officia
-  deserunt mollit anim id est laborum.'
+  deserunt mollit anim id est laborum."
+  STRING
 end
