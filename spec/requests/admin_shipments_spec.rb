e0d44f878491c36f72a7e8400a93d790f1b9da3f
jakrzus <jakub.krzus@gmail.com>
2021-08-03 18:45:08 +0100

    add shipment request check if not logged in as admin

--- spec/requests/admin_shipments_spec.rb
+++ spec/requests/admin_shipments_spec.rb
@@ -5,6 +5,7 @@ require './spec/support/helpers'
 
 RSpec.describe 'AdminShipments', type: :request do
   let!(:admin) { create :admin_user }
+  let!(:user) { create :user }
 
   headers = { 'ACCEPT' => 'application/json' }
 
@@ -43,6 +44,21 @@ RSpec.describe 'AdminShipments', type: :request do
 
       expect(JSON.parse(response.body)['state']).not_to eq('ready')
       expect(response.body).to include("Event 'prepare' cannot transition from 'failed'.")
+    end
+
+    it 'can not change shipment status if signed in  user is not an admin' do
+      sign_out admin
+      sign_in user
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+
+      expect(JSON.parse(response.body)['error']).to eq('You are not authorized')
+    end
+
+    it 'can not change shipment status if there is nobody signed in' do
+      sign_out admin
+      put "/admin/shipments/#{shipment.id}", params: { event: :prepare }, headers: headers
+
+      expect(JSON.parse(response.body)['error']).to eq('You need to sign in or sign up before continuing.')
     end
   end
 end
