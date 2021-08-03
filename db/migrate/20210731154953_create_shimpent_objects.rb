55ec8257d37cc4147504339449865b75b2a106dc
jakrzus <jakub.krzus@gmail.com>
2021-08-03 18:21:09 +0100

    Revert "create migration to adding creatingshipment for existing orders"
    This reverts commit 0fae435bd7bdf438b972e75b8a6e7663981bef55.

--- db/migrate/20210731154953_create_shimpent_objects.rb
+++
@@ -1,9 +0,0 @@
-class CreateShimpentObjects < ActiveRecord::Migration[6.1]
-  def up
-    Order.find_each(&:create_shipment)
-  end
-
-  def down
-    Order.find_each { |order| order.shipment.destroy }
-  end
-end
