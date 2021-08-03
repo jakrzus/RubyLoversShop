33e4277fca558d8cb633a4ca9d0dc93c79867588
jakrzus <jakub.krzus@gmail.com>
2021-08-03 17:32:26 +0100

    Revert "create payment for all orders"
    This reverts commit 1850523686450cf4d0326d8e8dba0a6b96db8493.

--- db/migrate/20210731152239_create_payment.rb
+++
@@ -1,9 +0,0 @@
-class CreatePayment < ActiveRecord::Migration[6.1]
-  def up
-    Order.find_each(&:create_payment)
-  end
-
-  def down
-    Order.find_each { |order| order.payment.destroy }
-  end
-end
