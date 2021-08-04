1fee3496b4dc003b5ddf46cb0ec86b353965e2f9
jakrzus <jakub.krzus@gmail.com>
2021-08-04 17:17:40 +0100

    change set status to return resource as payload

--- app/services/order_services/set_status.rb
+++ app/services/order_services/set_status.rb
@@ -3,9 +3,9 @@
 module OrderServices
   class SetStatus
     def call(resource, event)
-      response = resource.aasm.fire! event
+      resource.aasm.fire! event
       OpenStruct.new(flash: { type: :notice, message: "Succesfully changed #{name(resource)} status!" },
-                     payload: response)
+                     payload: resource)
     rescue AASM::InvalidTransition, ActiveModel::ValidationError => e
       OpenStruct.new(flash: { type: :alert, message: e.message }, payload: e)
     end
