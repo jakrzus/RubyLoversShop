e10e85b0a675035386b9bd8ff92a65a818422bd9
jakrzus <jakub.krzus@gmail.com>
2021-08-20 18:28:30 +0200

    create remove item service

---
+++ app/services/cart_services/remove_item.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module CartServices
+  class RemoveItem
+    def call(item, user)
+      cart_user = item.cart.user
+      return OpenStruct.new(success?: false, flash: 'Cart does not belong to this user!') if user != cart_user
+
+      if item.destroy!
+        OpenStruct.new(success?: true, flash: 'Item removed successfully')
+      else
+        OpenStruct.new(success?: false, flash: 'Item not removed successfully')
+      end
+    end
+  end
+end
