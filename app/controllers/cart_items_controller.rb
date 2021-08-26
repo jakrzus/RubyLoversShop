cbb429c8e80c1ba9dd5fb663735ccd164d9d0fb4
jakrzus <jakub.krzus@gmail.com>
2021-08-26 14:12:22 +0200

    add rescue and refactor cart_items controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,17 +1,26 @@
 class CartItemsController < ApplicationController
   def update
     item = CartItem.find(params[:id])
-    response = CartServices::UpdateItemQuantity.new.call(current_user, item, item_params)
-    if response.success?
-      redirect_to cart_path, notice: response.flash
-    else
-      redirect_to cart_path, alert: response.flash
+    begin
+      response = CartServices::UpdateItemQuantity.new.call(current_user, item, item_params)
+    rescue StandardError => e
+      redirect_to cart_path, alert: e.message
     end
+    set_flash(flash, response)
+    redirect_to cart_path
   end
 
   private
 
   def item_params
     params.require(:cart_item).permit(:quantity)
+  end
+
+  def set_flash(flash, response)
+    if response.success?
+      flash[:notice] = response.flash
+    else
+      flash[:alert] = response.flash
+    end
   end
 end
