19e9fa6f4a8992af95f9a0c38f791b36586b1047
jakrzus <jakub.krzus@gmail.com>
2021-08-26 14:16:42 +0200

    add error spec for update_item_quantity service spec

--- spec/services/update_item_quantity_spec.rb
+++ spec/services/update_item_quantity_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'UpdateItemQuantityService' do
 
   it 'does not remove an item from a cart if the user is not the cart owner' do
     response = update_item_quantity.call(unauthorized_user, item, params)
-    expect(response.success?).to be false
+    expect(response.success?).to be(false)
     expect(response.flash).to eq 'You are not authorized'
     expect(item.quantity).to eq(1)
   end
@@ -30,8 +30,15 @@ RSpec.describe 'UpdateItemQuantityService' do
     user = item.cart.user
     params = { quantity: '0' }
     response = update_item_quantity.call(user, item, params)
-    expect(response.success?).to be true
+    expect(response.success?).to be(true)
     expect(response.flash).to eq('Item removed successfully')
     expect(item.cart.cart_items).to be_empty
+  end
+
+  it 'does not update the item if there is no quantity in params' do
+    user = item.user
+    params = {}
+    expect { update_item_quantity.call(user, item, params) }.to raise_error('Quantity can not be nil')
+    expect(item.quantity).to eq(1)
   end
 end
