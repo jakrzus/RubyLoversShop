b47d2ec5946f74e31a4997034752488dc978346f
jakrzus <jakub.krzus@gmail.com>
2021-09-04 22:17:52 +0100

    remove bang from service objects save and update methods

--- app/services/cart_services/remove_item.rb
+++ app/services/cart_services/remove_item.rb
@@ -6,7 +6,7 @@ module CartServices
       cart_user = item.user
       return OpenStruct.new(success?: false, flash: 'Cart does not belong to this user!') if user != cart_user
 
-      if item.destroy!
+      if item.destroy
         OpenStruct.new(success?: true, flash: 'Item removed successfully')
       else
         OpenStruct.new(success?: false, flash: 'Item not removed successfully')
--- app/services/cart_services/update_item_quantity.rb
+++ app/services/cart_services/update_item_quantity.rb
@@ -7,7 +7,7 @@ module CartServices
       return OpenStruct.new(success?: false, flash: 'You are not authorized') unless authorized?(user, item)
       return CartServices::RemoveItem.new.call(item, user) if params[:quantity].to_i.zero?
 
-      if item.update!(params)
+      if item.update(params)
         OpenStruct.new(success?: true, flash: 'Item updated successfully')
       else
         OpenStruct.new(success?: false, flash: 'Could not update the item')
